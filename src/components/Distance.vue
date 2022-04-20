56c7548e45595bc1d2fb5e456a4129ce8e73a198
Houdaifa Amzil <houdaifaamzil@gmail.com>
2022-04-20 16:26:03 +0000

    Add multi  copy paste feature

--- src/components/Distance.vue
+++ src/components/Distance.vue
@@ -1,9 +1,36 @@
 <template>
-    <div class="flex flex-col justify-center items-center space-y-6 w-full">
-        <!-- <h1 class="text-4xl font-bold">F5atar ahssan chrikti</h1> -->
+    <div class="flex flex-col justify-center items-center space-y-6 w-full relative">
+        
+        <div class="px-4 py-3 absolute top-4 right-4">
+            <button @click="is_various = !is_various" type="button" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-gray-600 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+                <span v-if="is_various">Limité</span>
+                <span v-else>Plusieurs</span>
+            </button>
+        </div>
+
         <h1 class="text-3xl  font-extralight">Calculate the distance and time between two locations</h1>
 
-        <form class="space-y-8" @submit.prevent="AddToTable">
+        <form class="flex items-center justify-between space-x-12" @submit.prevent="Generate_Excel" v-if="is_various">
+
+                <div>
+                    <label for="origins" class="block text-sm font-medium text-gray-700"> Adresses de départ </label>
+                    <div class="mt-1">
+                        <textarea rows="10" cols="50" v-model="worker.origin" class="p-2 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border 
+                        border-gray-300 rounded-md" placeholder="Rue Jean Parot, 42100 Saint-Étienne, France"></textarea>
+                    </div>
+                </div>
+
+                <div>
+                    <label for="destinations" class="block text-sm font-medium text-gray-700"> Adresse d'arrivée </label>
+                    <div class="mt-1">
+                        <textarea rows="10" cols="50" v-model="worker.destination" class="p-2 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border 
+                        border-gray-300 rounded-md" placeholder="Rue de la Métare, 42100 Saint-Étienne, France"></textarea>
+                    </div>
+                </div>
+
+        </form>
+
+        <form class="space-y-8" @submit.prevent="AddToTable" v-else>
             <div class="">
                 <input type="text" v-model="worker.name" placeholder="Name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm border-gray-300 rounded-md">
             </div>
@@ -55,7 +82,7 @@
 
         </form>
 
-        <table class="min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-700 text-center">
+        <table class="min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-700 text-center" v-if="!is_various">
             <thead class="bg-gray-100 dark:bg-gray-700">
                 <tr>
                     <th>Name</th>
@@ -98,7 +125,7 @@ export default {
     name: 'Distance',
     data(){
         return{
-            worker: {name: '', origin: '', destination: '', duration: ''},
+            worker: {name: '', origin: '', destination: ''},
             workers:[],
             travel_modes: ["DRIVING", "BICYCLING"],
 
@@ -113,6 +140,8 @@ export default {
                     }
                 ]
             ],
+
+            is_various: false
         }
     },
     computed: {
@@ -121,7 +150,7 @@ export default {
     methods:{
         AddToTable(){
             if(this.worker.name === "" || this.worker.origin === "" || this.worker.destination === "") return
-            this.workers.push({name: this.worker.name, origin: this.worker.origin, destination: this.worker.destination, duration: this.worker.duration});
+            this.workers.push({name: this.worker.name, origin: this.worker.origin, destination: this.worker.destination});
         },
         remove_worker(worker_index){
             this.workers = this.workers.filter((w, key) => key !== worker_index);
@@ -135,48 +164,101 @@ export default {
         Generate_Excel(){
             var _this = this;
 
-            if(this.workers.length <= 0 || this.travel_modes.length <= 0 ) return
-
             var directionsService = new this.google.maps.DirectionsService();
 
             var request = {};
             var dt = [];
+            
+            if(this.is_various){
 
-            _this.workers.forEach(worker => {
+                let arr = [];
 
-                _this.travel_modes.forEach(mode => {
+                for (const [key, value] of Object.entries(this.worker)) {
+                    console.log(key);
+                    let v = value.split("\n");
+                    arr.push((v.filter(Boolean)));
+                }
+                
+                let origins = arr[1];
+                let destinations = arr[2];
+
+                for (let i = 0; i < origins.length; i++) {
+
+                    if(origins[i] === undefined || destinations[i] === undefined) return
                     
-                    //create request
-                    request = {
-                        origin: worker.origin.formatted_address,
-                        destination: worker.destination.formatted_address,
-                        travelMode: mode, //WALKING, BYCYCLING, TRANSIT
-                        unitSystem: this.google.maps.UnitSystem.IMPERIAL
-                    }
-
-                    // // pass the request to the route method
-                    directionsService.route(request, function (result, status) {
-                        if(status === "OK"){
-
-                            let distance_in_kilimoters = parseInt(result.routes[0].legs[0].distance.text) * 1.6;
-
-                            dt.push({Name: worker.name, Origin: worker.origin.name, 
-                                    Destination: worker.destination.name,
-                                    Distance: distance_in_kilimoters + " Kilometers",
-                                    Duration: result.routes[0].legs[0].duration.text,
-                                    Mode: mode});
-
-                            _this.file_ready = true;
-                        }else{
-                            alert(status + " of "+ worker.name +"!\nPlease choose others locations");
+                    _this.travel_modes.forEach(mode => {
+                        
+                        request =  {
+                            origin: origins[i],
+                            destination: destinations[i],
+                            travelMode: mode, //WALKING, BYCYCLING, TRANSIT
+                            unitSystem: this.google.maps.UnitSystem.IMPERIAL
                         }
-                    });
 
+                        // // pass the request to the route method
+                        directionsService.route(request, function (result, status) {
+                            if(status === "OK"){
+
+                                let distance_in_kilimoters = parseInt(result.routes[0].legs[0].distance.text) * 1.6;
+
+                                dt.push({Name: "", Origin: origins[i], 
+                                        Destination: destinations[i],
+                                        Distance: distance_in_kilimoters + " Kilometers",
+                                        Duration: result.routes[0].legs[0].duration.text,
+                                        Mode: mode});
+
+                                _this.file_ready = true;
+                            }else{
+                                // console.log(status + " of "+ worker.name +"!\nPlease choose others locations");
+                            }
+                        });
+
+                    });
+                    
+                }
+
+            }else{
+
+                if(this.workers.length <= 0 || this.travel_modes.length <= 0 ) return
+
+                _this.workers.forEach(worker => {
+
+                    _this.travel_modes.forEach(mode => {
+                        
+                        //create request
+                        request = {
+                            origin: worker.origin.formatted_address,
+                            destination: worker.destination.formatted_address,
+                            travelMode: mode, //WALKING, BYCYCLING, TRANSIT
+                            unitSystem: this.google.maps.UnitSystem.IMPERIAL
+                        }
+
+                        // // pass the request to the route method
+                        directionsService.route(request, function (result, status) {
+                            if(status === "OK"){
+
+                                let distance_in_kilimoters = parseInt(result.routes[0].legs[0].distance.text) * 1.6;
+
+                                dt.push({Name: worker.name, Origin: worker.origin.formatted_address, 
+                                        Destination: worker.destination.formatted_address,
+                                        Distance: distance_in_kilimoters + " Kilometers",
+                                        Duration: result.routes[0].legs[0].duration.text,
+                                        Mode: mode});
+
+                                _this.file_ready = true;
+                            }else{
+                                alert(status + " of "+ worker.name +"!\nPlease choose others locations");
+                            }
+                        });
+
+                    });
                 });
-            });
+
+            }
 
             _this.json_data = dt;
-        },
+
+        }
     }
 }
 </script>
